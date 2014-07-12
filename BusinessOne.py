a0eabae70b854c73f1f46e794529ca46a9ed7c9a
ljtheminister <john.c.min@gmail.com>
2014-07-11 23:32:53 -0400

    BusinessOne - Overall data for each year by channel for lives and lives_Rx

--- BusinessOne.py
+++ BusinessOne.py
@@ -3,6 +3,38 @@ import numpy as np
 
 data = pd.read_csv('Raw Data File for Business One.csv')
 
+channel_data = data.groupby('Channel')
+
+
+
+all_years = {}
+
+for channel, channel_grp in channel_data:
+    all_lives = 0
+    rx_lives = 0 
+    entities = channel_grp.groupby(['Entity_Name', 'Period_Year'])
+    for entity, entity_grp in entities:
+        all_lives += entity_grp['Total_Lives'].mean()
+        rx_lives += entity_grp['Total_Lives_Rx'].mean()
+    all_years[channel] = (all_lives, rx_lives)
+
+
+
+yearly = {}
+
+grouped_data = data.groupby(['Period_Year', 'Channel']
+
+for channel_year, group in grouped_data:
+    all_lives = 0
+    rx_lives = 0
+    entities = group.groupby(['Entity_Name'])
+    for entity, entity_grp in entities:
+        all_lives += entity_grp['Total_Lives'].mean()
+        rx_lives += entity_grp['Total_Lives_Rx'].mean()
+    yearly[channel_year] = (all_lives, rx_lives) 
+    
+
+
 
 
 
--- Fingertip.py
+++ Fingertip.py
@@ -1,6 +1,106 @@
 import pandas as pd
 import numpy as np
 
+import openpyxl
+import xlrd
+
+
+book = xlrd.open_workbook('ONCOLOGY Report July2014.xlsx')
+
+print data.sheet_names()
+
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
 
 
 
+def process_worksheet(worksheet):
+
+num_rows = worksheet.nrows
+curr_row = 10
+
+while curr_row < num_rows:
+    row = worksheet.row(curr_row)
+    if row[0].value != 'Reason Codes':
+        print row[0]
+        curr_row += 1
+    else:
+        break
+
+reason_codes = {}
+curr_row += 1
+while curr_row < num_rows:
+    row = worksheet.row(curr_row)
+    code, reason = row[0].value.split('- ')
+    reason_codes[code] = reason
+    print row
+    curr_row += 1
+
+
+
+
+
+
+
+
+
+worksheets = workbook.sheet_names()
+for worksheet_name in worksheets:
+    worksheet = workbook.sheet_by_name(worksheet_name)
