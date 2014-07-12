905014271ee1f03ce3a743bf9d14bbb945cb0f5a
ljtheminister <john.c.min@gmail.com>
2014-07-12 01:28:28 -0400

    finished BusinessOne-Overall, AllLives, RxLives, working on Copay

--- BusinessOne.py
+++ BusinessOne.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 data = pd.read_csv('Raw Data File for Business One.csv')
 
-channel_data = data.groupby('Channel')
 
-
-
+# Life count by channel
 all_years = {}
-
+channel_data = data.groupby('Channel')
 for channel, channel_grp in channel_data:
     all_lives = 0
     rx_lives = 0 
@@ -19,11 +18,9 @@ for channel, channel_grp in channel_data:
     all_years[channel] = (all_lives, rx_lives)
 
 
-
+# Life count by channel/year
 yearly = {}
-
-grouped_data = data.groupby(['Period_Year', 'Channel']
-
+grouped_data = data.groupby(['Period_Year', 'Channel'])
 for channel_year, group in grouped_data:
     all_lives = 0
     rx_lives = 0
@@ -34,11 +31,56 @@ for channel_year, group in grouped_data:
     yearly[channel_year] = (all_lives, rx_lives) 
     
 
+'''
+Do I have to clean-up the tier data?
+'''
+
+
+def compute_lives(data, all_dict, rx_dict, key1, key2):
+    all_lives = 0
+    rx_lives = 0
+    entities = data.groupby('Entity_Name')
+    for entity, entity_grp in entities:
+        all_lives += entity_grp['Total_Lives'].mean()
+        rx_lives += entity_grp['Total_Lives_Rx'].mean()
+    all_dict[key1][key2] = all_lives
+    rx_dict[key1][key2] = rx_lives
+ 
+
+
+all_lives_tier = defaultdict(dict)
+rx_lives_tier = defaultdict(dict)
+
+
+for channel_year, group in grouped_data:
+    print channel_year
+    tiers = group.groupby(['Form_Status'])
+    for tier, tier_data in tiers:
+        print tier
+        PA_only = tier_data.query("Restrict_PA=='Y'")
+        compute_lives(PA_only, all_lives_tier, rx_lives_tier, channel_year, 'PA_only')
+        PA_plus = tier_data.query("Restrict_PA=='Y' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
+        compute_lives(PA_plus, all_lives_tier, rx_lives_tier, channel_year, 'PA_plus')
+        Non_PA = tier_data.query("Restrict_PA=='-' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
+        compute_lives(Non_PA, all_lives_tier, rx_lives_tier, channel_year, 'Non_PA')
+
+
+
+
+group = grouped_data.get_group((2010, 'Commercial'))
+tiers = group.groupby(['Form_Status'])
+tier_data = tiers.get_group('Tier 2')
 
 
 
 
 
+for i, row in Non_PA[['Restrict_PA', 'Restrict_SE', 'Restrict_QL', 'Restrict_AR']].iterrows():
+    print row
+
+
+
+#PA_plus[['Restrict_PA', 'Restrict_SE', 'Restrict_QL', 'Restrict_AR']]
 
 
 
