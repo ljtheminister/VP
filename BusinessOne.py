1063ed22a4d7f609a900ad6f88e882f1416bb9a5
ljtheminister <john.c.min@gmail.com>
2014-07-15 03:26:45 -0400

    Finished BusinessOne analysis

--- BusinessOne.py
+++ BusinessOne.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 from decimal import Decimal
+import xlwt
+import csv
+import cPickle as pickle
 
 data = pd.read_csv('Raw Data File for Business One.csv')
 
@@ -33,6 +36,32 @@ for channel_year, group in grouped_data:
     yearly[channel_year] = (all_lives, rx_lives) 
     
 
+yearly_lives = defaultdict(dict)
+for year, channel in yearly.keys():
+    yearly_lives[year][channel] = yearly[(year,channel)]
+
+channels = ['Commercial', 'DoD', 'Managed Medicaid', 'Medicare', 'PBM Commercial', 'PBM Medicare', 'State Medicaid', 'VA']
+
+
+with open('BusinessOne_Overall.csv', 'wb') as f:
+    csvwriter = csv.writer(f, delimiter=',')
+    csvwriter.writerow(['All Years'])
+    csvwriter.writerow(['', 'All Lives', 'Rx Lives'])
+    for channel in channels:
+        all_lives, rx_lives = all_years[channel]
+        csvwriter.writerow([channel, int(all_lives), int(rx_lives)])
+
+    for year in yearly_lives.keys():
+        csvwriter.writerow([])
+        csvwriter.writerow([str(year)])
+        csvwriter.writerow(['', 'All Lives', 'Rx Lives'])
+        for channel in channels: 
+            try:
+                all_lives, rx_lives = yearly_lives[year][channel] 
+            except: 
+                all_lives, rx_lives = 0, 0
+            csvwriter.writerow([channel, int(all_lives), int(rx_lives)]) 
+
 '''
 Do I have to clean-up the tier data?
 '''
@@ -49,7 +78,7 @@ def compute_lives(data, all_dict, rx_dict, key1, key2):
     rx_dict[key1][key2] = rx_lives
  
 
-def compute_lives_copay(data, all_dict, rx_dict, copay_dict, key1, key2):
+def compute_lives_copay(data, tiered_data, year, channel, tier, restriction):
     all_lives = 0
     rx_lives = 0
     copay = 0
@@ -62,26 +91,140 @@ def compute_lives_copay(data, all_dict, rx_dict, copay_dict, key1, key2):
             pass
         else:
             copay += (entity_grp.copay * entity_grp.Total_Lives_Rx).mean()/rx_lives_sum
+
+    restrict_dict = tiered_data[year][channel][tier][restriction] 
+    restrict_dict['All Lives'] = all_lives
+    restrict_dict['Rx Lives'] = rx_lives
+    restrict_dict['Copay'] = copay
+    all_plans = tiered_data[year]['All Plans'][tier][restriction]
+    all_plans['All Lives'] += all_lives
+    all_plans['Rx Lives'] += rx_lives
+    all_plans['Copay'] += copay
+
+
+'''
     all_dict[key1][key2] = all_lives
     rx_dict[key1][key2] = rx_lives
     copay_dict[key1][key2] = copay
-
-all_lives_tier = defaultdict(dict)
+all_
 rx_lives_tier = defaultdict(dict)
 avg_copay = defaultdict(dict)
+'''
 
-for channel_year, group in grouped_data:
-    print channel_year
-    tiers = group.groupby(['Form_Status'])
-    for tier, tier_data in tiers:
-        print tier
-        PA_only = tier_data.query("Restrict_PA=='Y'")
-        compute_lives_copay(PA_only, all_lives_tier, rx_lives_tier, avg_copay, channel_year, 'PA_only')
-        PA_plus = tier_data.query("Restrict_PA=='Y' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
-        compute_lives_copay(PA_plus, all_lives_tier, rx_lives_tier, avg_copay, channel_year, 'PA_plus')
-        Non_PA = tier_data.query("Restrict_PA=='-' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
-        compute_lives_copay(Non_PA, all_lives_tier, rx_lives_tier, avg_copay, channel_year, 'Non_PA')
+years = sorted(data['Period_Year'].unique())
+channels = sorted(data['Channel'].unique())
+tiers = sorted(data['Form_Status'].unique())
+restrictions = ['PA only', 'PA plus', 'Non-PA', 'No Restrictions']
+items = ['All Lives', 'Rx Lives', 'Copay']
 
+def instantiate_tiered_data():
+    tiered_data = {}
+    channels.append('All Plans')
+    for year in years:
+        tiered_data[year] = {}
+        t_year = tiered_data[year]
+        for channel in channels:
+            t_year[channel] = {}
+            t_channel = t_year[channel]
+            for tier in tiers:
+                t_channel[tier] = {}
+                t_tier = t_channel[tier]
+                for restriction in restrictions:
+                    t_tier[restriction] = {}
+                    t_restriction = t_tier[restriction]
+                    for item in items:
+                        t_restriction[item] = 0
+    channels.pop()
+    return tiered_data 
+
+def compute_tier_counts():
+    tiered_data = instantiate_tiered_data()
+    years_group = data.groupby(['Period_Year'])
+
+    for year, year_data in years_group:
+        print year
+        channels_group = year_data.groupby(['Channel'])
+
+        for channel, channel_data in channels_group:
+            print channel
+            tiers_group = channel_data.groupby(['Form_Status'])
+
+            for tier, tier_data in tiers_group:
+                print tier
+                PA_only = tier_data.query("Restrict_PA=='Y'")
+                compute_lives_copay(PA_only, tiered_data, year, channel, tier, 'PA only')
+
+                PA_plus = tier_data.query("Restrict_PA=='Y' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
+                compute_lives_copay(PA_plus, tiered_data, year, channel, tier, 'PA plus')
+
+                Non_PA = tier_data.query("Restrict_PA=='-' and (Restrict_SE=='Y' or Restrict_QL=='Y' or Restrict_AR=='Y')")
+                compute_lives_copay(Non_PA, tiered_data, year, channel, tier, 'Non-PA')
+
+                No_restrictions = tier_data.query("Restrict_PA=='-' and Restrict_SE=='-' and Restrict_QL=='-' and Restrict_AR=='-'")
+                compute_lives_copay(No_restrictions, tiered_data, year, channel, tier, 'No Restrictions')
+    return tiered_data
+
+
+tiered_data = compute_tier_counts()
+
+pickle.dump(tiered_data, open('tiered_BusinessOne.p', 'wb'))
+
+# Write out tiered results to csv
+
+#tier_labels = ['', '', 'Tier 1', '', '', 'Tier 2', '', '', 'Tier 3', '', '', 'Tier 4', '', '', 'Tiers 5-8', '']
+#restrictions_labels = ['', 'PA Only', 'PA +', 'R (Non-PA)', 'PA Only', 'PA +', 'R (Non-PA)', 'PA Only', 'PA +', 'R (Non-PA)', 'PA Only', 'PA +', 'R (Non-PA)', 'PA Only', 'PA +', 'R (Non-PA)']
+
+tier_labels = ['', 'Tier 1', '', '', '',  'Tier 2', '', '', '',  'Tier 3', '', '', '',  'Tier 4', '', '', '',  'Tier 5-8', '', '', '']
+
+restrictions_labels = ['', 'PA Only', 'PA +', 'R (Non-PA)', 'No Restrictions', 'PA Only', 'PA +', 'R (Non-PA)', 'No Restrictions', 'PA Only', 'PA +', 'R (Non-PA)', 'No Restrictions', 'PA Only', 'PA +', 'R (Non-PA)', 'No Restrictions', 'PA Only', 'PA +', 'R (Non-PA)', 'No Restrictions']
+
+
+tiers_of_interest = ['Tier 1', 'Tier 2', 'Tier 3', 'Tier 4', 'Tier 5-8']
+
+def aggregate_channel_data(data, year, channel, item):
+    channel_data = [channel]
+    d = data[year][channel]
+    for tier in tiers_of_interest:
+        for restriction in restrictions:
+            channel_data.append(d[tier][restriction][item])
+    return channel_data
+    
+
+def write_tiered_data(filename, tiered_data, item):
+    with open('%s.csv'%(filename), 'wb') as f:
+        csvwriter = csv.writer(f, delimiter=',')
+        for year in years:
+            csvwriter.writerow([str(year)])
+            csvwriter.writerow(tier_labels)
+            csvwriter.writerow(restrictions_labels)
+            for channel in channels:
+                channel_data = aggregate_channel_data(tiered_data, year, channel, item)
+                csvwriter.writerow(channel_data)
+            all_plans_sum = aggregate_channel_data(tiered_data, year, 'All Plans', item)
+            csvwriter.writerow(all_plans_sum)
+            csvwriter.writerow([])
+
+
+write_tiered_data('BusinessOne_All_Lives', tiered_data, 'All Lives')
+write_tiered_data('BusinessOne_Rx_Lives', tiered_data, 'Rx Lives')
+write_tiered_data('BusinessOne_Copay', tiered_data, 'Copay')
+
+
+
+csv_list = ['BusinessOne_Overall', 'BusinessOne_All_Lives', 'BusinessOne_Rx_Lives', 'BusinessOne_Copay']
+
+
+workbook = xlwt.Workbook()
+for csv_name in csv_list:
+    f = open(csv_name + '.csv', 'rb')
+    g = csv.reader(f, delimiter=',')
+    sheet = workbook.add_sheet(csv_name) 
+
+    for i, row in enumerate(g):
+        for j, value in enumerate(row):
+            sheet.write(i, j, value)
+
+workbook.save('BusinessOne.xls')
 
 '''
 sub = data.query("Channel=='Commercial' and Form_Status=='Tier 2' and Restrict_PA=='Y'")
@@ -91,4 +234,20 @@ tier_data = tiers.get_group('Tier 2')
 for i, row in Non_PA[['Restrict_PA', 'Restrict_SE', 'Restrict_QL', 'Restrict_AR']].iterrows():
     print row
 #PA_plus[['Restrict_PA', 'Restrict_SE', 'Restrict_QL', 'Restrict_AR']]
+'''
+'''
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet('BusinessOne-Overall')
+
+sheet.write(0, 0, 'All Years')
+sheet.write(1, 1, 'All Lives')
+sheet.write(1, 2, 'Rx Lives')
+
+row_num = 2
+for channel in channels:
+    all_lives, rx_lives = all_years[channel]
+    sheet.write(row_num, 0, '%s, %s, %s'%(channel, str(all_lives), str(rx_lives)))
+    row_num += 1
+
+workbook.save('BusinessOne.xls')
 '''
--- Fingertip.py
+++ Fingertip.py
@@ -5,7 +5,7 @@ import openpyxl
 import xlrd
 
 
-book = xlrd.open_workbook('ONCOLOGY Report July2014.xlsx')
+workbook = xlrd.open_workbook('ONCOLOGY Report July2014.xlsx')
 
 print data.sheet_names()
 
@@ -69,38 +69,215 @@ states = {
         'WY': 'Wyoming'
 }
 
+months = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12
+}
+
 
 
 def process_worksheet(worksheet):
 
-num_rows = worksheet.nrows
-curr_row = 10
+    name = worksheet.name
+    year = name[-4:]
+    month = months[name[:-4][:3]]
 
-while curr_row < num_rows:
-    row = worksheet.row(curr_row)
-    if row[0].value != 'Reason Codes':
-        print row[0]
-        curr_row += 1
+    num_rows = worksheet.nrows
+    '''
+    if worksheet.name == 'July2014':
+        curr_row = 9
+        header = worksheet.row(curr_row)
+        header = [str(x.value) for x in header]
     else:
-        break
+    '''
+    curr_row = 10
+    header = worksheet.row(curr_row)
+    header = [str(x.value) for x in header]
 
-reason_codes = {}
-curr_row += 1
-while curr_row < num_rows:
-    row = worksheet.row(curr_row)
-    code, reason = row[0].value.split('- ')
-    reason_codes[code] = reason
-    print row
-    curr_row += 1
+    table = []
+
+    while curr_row < num_rows:
+        curr_row += 1
+        row = worksheet.row(curr_row)
+        row = [str(x.value) for x in row]
+        if row[0] != '':
+            table.append(row)
+        else:
+            break
+    '''
+    reason_codes = {}
+    curr_row += 2 #skip "Reason Codes" row
+    while curr_row < num_rows:
+        row = worksheet.row(curr_row)
+        row = [str(x.value) for x in row]
+        code, reason = row[0].split('- ')
+        reason_codes[str(code)] = str(reason)
+        curr_row += 1
+    '''
+    df = pd.DataFrame(table)
+    df.columns = header
+    df['Month'] = month
+    df['Year'] = year
+    return df
+    #return df, reason_codes
 
 
-
-
-
-
-
-
+df_list = []
 
 worksheets = workbook.sheet_names()
 for worksheet_name in worksheets:
     worksheet = workbook.sheet_by_name(worksheet_name)
+    df = process_worksheet(worksheet)
+    df_list.append(df)
+
+data = pd.concat(df_list, ignore_index=True)
+data.to_csv('Fingertip_agg.csv', index=False)
+
+
+data = pd.read_csv('Fingertip_agg.csv')
+data['Plan Type'].value_counts()
+
+for year, grp in data.groupby('Year'):
+    print year
+    print grp['Plan Type'].value_counts()
+
+
+
+
+for state in states.values():
+    data[state] = 'No'
+
+for i, row in data.iterrows():
+    print i
+    states_list = row['State(s) of Operation'].split(',')
+    if states_list[0] != '':
+        for state in states_list:
+            state_abbrev = state.split()[0]
+            data[states[state_abbrev]] = 'Yes'
+    else:
+        pass
+
+
+
+
+drug['PA'] = 'No'
+drug['QL'] = 'No'
+drug['ST'] = 'No'
+drug['OR'] = 'No'
+
+
+
+
+drug_name = 'Tarceva'
+
+def filter_drug(data, drug_name):
+    drug = data[['Health Plan', 'Plan Type', drug_name]]
+    print len(drug)
+    drug['PA'] = 'No'
+    drug['QL'] = 'No'
+    drug['ST'] = 'No'
+    drug['OR'] = 'No'
+    drug['Tier'] = np.nan
+
+    for i, row in drug.iterrows():
+        print i
+        d = row[drug_name]
+
+        if '(PA)' in d: 
+            drug.loc[i, 'PA'] = 'Yes'
+
+        if '(QL)' in d:
+            drug.loc[i, 'QL'] = 'Yes'
+
+        if '(ST)' in d:
+            drug.loc[i, 'ST'] = 'Yes'
+
+        if '(OR)' in d:
+            drug.loc[i, 'OR'] = 'Yes'
+
+        if 'Tier' in d:
+            tier = d[5]
+            drug.loc[i, 'Tier'] = tier
+        elif 'NC' in d:
+            drug.loc[i, 'Tier'] = 'NC'
+
+        elif 'N/A' in d:
+            drug.loc[i, 'Tier'] = 'N/A'
+
+    return drug
+
+tarceva = filter_drug(data, 'Tarceva')
+
+tarceva.to_csv('Fingertip_Tarceva.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tarceva = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
